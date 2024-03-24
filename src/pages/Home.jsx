51b25def61ad8f562261e562a11ac3c141540043
Jeferson Fernandes <jeferson.fernandes@maxiconsystems.com.br>
2024-03-24 10:08:50 -0300

    INIT: initial setup with react, vitejs and tailwindcss

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="container">
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold text-zinc-50">Hello World!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
