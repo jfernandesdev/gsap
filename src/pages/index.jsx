591f3f8a4e39d5aefb01e138024692463d7d6889
Jeferson Fernandes <jeferson.fernandes@maxiconsystems.com.br>
2024-03-25 20:06:32 -0300

    FEAT: gsap.to, gsap.from, gsap.fromTo, gsap.timeline, gsap.stagger and gsap.text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,19 @@
 import Home from "./Home";
+import GsapTo from "./GsapTo";
+import GsapFrom from "./GsapFrom";
+import GsapFromTo from "./GsapFromTo";
+import GsapStagger from "./GsapStagger";
+import GsapTimeline from "./GsapTimeline";
+import GsapScrollTrigger from "./GsapScrollTrigger";
+import GsapText from "./GsapText";
 
 export {
-  Home
+  GsapTo,
+  GsapFrom,
+  GsapFromTo,
+  Home,
+  GsapTimeline,
+  GsapScrollTrigger,
+  GsapStagger,
+  GsapText,
 };
